34f6476677dc55bbbd7dd5080f1d15553ac80978
Magalhaes <marmagalhaes@deloitte.com>
2022-03-08 14:08:07 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -67,6 +67,12 @@ export default ()=>{
                 Direitos de imagem para Netflix, HBO, Amaxon Prime Video.<br/>
                 API do site Themoviedb.org.
             </footer>
+            
+            {movieList.length <=0 &&
+            <div className="loading">
+                <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"/>
+            </div>
+            }
         </div>
     );
 }
