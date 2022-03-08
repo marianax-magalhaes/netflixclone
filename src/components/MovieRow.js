717107e644c172bfab791c92416e518f6413b238
Magalhaes <marmagalhaes@deloitte.com>
2022-03-08 13:59:26 -0300

    scroll lateral das listas de filmes

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css';
 
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
@@ -6,21 +6,39 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items})=>{
 
+    const [scrollX, setSrcollx] = useState(-400);
+    const handleLeftArrow = () => {
+         let x = scrollX + Math.round(window.innerWidth/2);
+         if(x > 0){
+             x=0;
+         } setSrcollx(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth/2);
+        let listW = items.results.length * 150;
+
+        if((window.innerWidth - listW) > x){
+            x = window.innerWidth - listW - 60;
+        }
+        setSrcollx(x);
+    }
+
     return(
         <div className="movieRow">
             <h2>{title}</h2>
             
-            <div className="movieRow--left">
-                <NavigateBeforeIcon style={{fontsize:15}}/>
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize:50}}/>
             </div>
 
-            <div className="movieRow--left">
-                <NavigateNextIcon style={{fontsize:15}}/>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <NavigateNextIcon style={{fontSize:50}}/>
             </div>
 
             <div className="movieRow--listarea">
 
-                <div className="movieRow--list ">
+                <div className="movieRow--list " style={{marginLeft:scrollX, width: items.results.length*150}}>
 
                     {items.results?.length > 0 && items.results.map((item, key)=> ( 
                         <div key={key} className="movieRow--item">
